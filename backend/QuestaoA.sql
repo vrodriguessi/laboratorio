56fb6eb15517b27a9569efd132636241e10ecaab
Veronica <veronicarodri2001@gmail.com>
2024-08-18 12:57:54 -0300

    QuestãoA, modificação

--- backend/QuestaoA.sql
+++ backend/QuestaoA.sql
@@ -1,25 +1,25 @@
 -- Adicionar coluna
-ALTER TABLE 'Paciente' 
-ADD COLUMN 'DataNascimento DATE NULL';
+ALTER TABLE `Paciente`
+ADD COLUMN `DataNascimento` DATE NULL;
 
 -- Modificar coluna
-ALTER TABLE 'Funcionario'
-MODIFY COLUMN 'Salario' DECIMAL(10,2) NOT NULL;
+ALTER TABLE `Funcionario`
+MODIFY COLUMN `Salario` DECIMAL(10,2) NOT NULL;
 
 -- Renomear coluna
-ALTER TABLE 'Solicitante'
-CHANGE COLUMN 'Logradouro' 'Endereco' VARCHAR(50) NOT NULL;
+ALTER TABLE `Solicitante`
+CHANGE COLUMN `Logradouro` `Endereco` VARCHAR(50) NOT NULL;
 
 
 ----------------------------------------------------------
 ----------------------------------------------------------
 
 -- Criar tabela extra
-CREATE TABLE IF NOT EXISTS 'ExtraTable' (
-    'Id' INT AUTO_INCREMENT NOT NULL,
-    'Descricao' VARCHAR(100),
-    PRIMARY KEY ('Id')
-); ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `ExtraTable` (
+    `Id` INT AUTO_INCREMENT NOT NULL,
+    `Descricao` VARCHAR(100) NULL,
+    PRIMARY KEY (`Id`)
+) ENGINE=InnoDB;
 
 -- Apagar tabela extra
-DROP TABLE IF EXISTS 'ExtraTable';
+DROP TABLE IF EXISTS `ExtraTable`;
