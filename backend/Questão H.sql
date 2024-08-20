ac51d91f7fbabaec5e8357e72efc4444ac69167b
ErikaMara <erikamara.machado@gmail.com>
2024-08-19 23:43:06 -0300

    Questao H

--- backend/Questão H.sql
+++ backend/Questão H.sql
@@ -2,10 +2,17 @@
 CREATE USER 'usuario1'@'localhost' IDENTIFIED BY 'senha1';
 CREATE USER 'usuario2'@'localhost' IDENTIFIED BY 'senha2';
 
+SELECT User, Host FROM mysql.user;
+
 -- Concessão de permissões
 GRANT SELECT, INSERT, UPDATE ON `mydb`.* TO 'usuario1'@'localhost';
 GRANT ALL PRIVILEGES ON `mydb`.* TO 'usuario2'@'localhost';
 
+SHOW GRANTS FOR 'usuario1'@'localhost';
+SHOW GRANTS FOR 'usuario2'@'localhost';
+
 -- Revogação de permissões
 REVOKE INSERT, UPDATE ON `mydb`.* FROM 'usuario1'@'localhost';
-REVOKE ALL PRIVILEGES ON `mydb`.* FROM 'usuario2'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'usuario2'@'localhost';
+
+
